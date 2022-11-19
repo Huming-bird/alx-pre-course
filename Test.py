aebfb578422dda994f86b45455d2522722a74fa9
sonuga <ahmedolalekan6@gmail.com>
2022-11-19 17:55:54 +0100

    -testing- i removed the python file

--- Test.py
+++
@@ -1,9 +0,0 @@
-#for i in range(10):
-    #print(i)
-data = input('please enter a range of data: \n')
-for num in data:
-    try:
-        num = int(num)
-        print(num, num*num, sep=' ')
-    except ValueError as e:
-        pass
