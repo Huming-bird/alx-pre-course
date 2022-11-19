ab06f8fab603a4fb55717731b51008813f266613
sonuga <ahmedolalekan6@gmail.com>
2022-11-19 17:49:01 +0100

    -testing-

--- Test.py
+++ Test.py
@@ -1,5 +1,9 @@
-for i in range(10):
-    print(i)
-data = int(input('please enter a range of data: \n')
+#for i in range(10):
+    #print(i)
+data = input('please enter a range of data: \n')
 for num in data:
-    print(num, num*num, sep=' ')
+    try:
+        num = int(num)
+        print(num, num*num, sep=' ')
+    except ValueError as e:
+        pass
